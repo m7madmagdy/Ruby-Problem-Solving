665bad4bfac6d9087c08185c1077bd1bf8925459
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-07-16 14:37:05 +0200

    change packages

--- basics/hashes.rb
+++
@@ -1,9 +0,0 @@
-#store key and value in hash
-
-states = {
-    "Egypt" => "EG",
-    "Moroco" => "MA",
-    "Sudan" => "SD"
-}
-
-puts states["Egypt"]
--- basics/methods.rb
+++
@@ -1,5 +0,0 @@
-def method1(name, age)
-    puts ("Your name is #{name}")
-    puts ( "your age is " + age.to_s)
-end
-puts method1("MEGO", 22)
