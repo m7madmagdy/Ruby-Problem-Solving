e6b5308d837725d1594b961431d90737ed42bfa2
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-08-09 23:20:28 +0200

    4 tasks hackerRank

---
+++ hackerRank/Array - Addition.rb
@@ -0,0 +1,21 @@
+#Link: https://www.hackerrank.com/challenges/ruby-array-addition/problem?isFullScreen=true
+
+def end_arr_add(arr, element)
+  # Add `element` to the end of the Array variable `arr` and return `arr`
+  arr.push(element)
+end
+
+def begin_arr_add(arr, element)
+  # Add `element` to the beginning of the Array variable `arr` and return `arr`
+  arr.insert(0,element)
+end
+
+def index_arr_add(arr, index, element)
+  # Add `element` at position `index` to the Array variable `arr` and return `arr`
+  arr.insert(index, element)
+end
+
+def index_arr_multiple_add(arr, index)
+  # add any two elements to the arr at the index
+  arr.unshift(index, 2)
+end
