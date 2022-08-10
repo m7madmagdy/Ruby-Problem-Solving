39b4446c23d6e911020330398c48c24b07e810ee
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-08-10 14:02:37 +0200

    4 tasks code wars

--- codewars/Is-It-Even?/Solution.rb
+++ codewars/Is-It-Even?/Solution.rb
@@ -1,3 +1,7 @@
+
+#Link: https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/ruby
+
+#Solution
 def test_even(n)
   n % 2 == 0
   # or
---
+++ codewars/Messi-Goals-Function/Solution.rb
@@ -0,0 +1,9 @@
+#Link: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/ruby
+
+
+#Solution
+def goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals)
+  laLigaGoals + copaDelReyGoals + championsLeagueGoals
+end
+
+puts goals(10, 5, 20)
