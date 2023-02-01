64e03e0c3898b5c879e2d9f3cdde7b8b607a7619
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-02-01 15:12:08 +0200

    refactor

--- codewars/6 kyu/Write Number in Expanded Form/Solution.rb
+++ codewars/6 kyu/Write Number in Expanded Form/Solution.rb
@@ -1,7 +1,7 @@
 def expanded_form(num)
   num.to_s.split('').reverse.each_with_index.map { |num, index|
     num + '0' * index if num != '0'
-  }.reverse.join(' + ')
+  }.reverse.compact.join(' + ')
 end
 
-puts expanded_form(433)
+puts expanded_form(11111)
